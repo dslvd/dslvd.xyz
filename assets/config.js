f96c851eac6160290c4dd970de5b2099e71a6902
dslvd <80934874+dslvd@users.noreply.github.com>
2022-04-25 21:29:04 +0800

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,27 @@
+let config = {
+  "Title": "dslvd - links",
+  "Name": "dslvd",
+  "Description": "This website/domain is for my links and redirects :)",
+  "links": [
+    {
+      "Title": "Website",
+      "URL": "http://www.dslvd.com",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Github",
+      "URL": "https://github.com/dslvd",
+      "icon_classes": "fab fa-github",
+    },
+    {
+      "Title": "Twitter",
+      "URL": "https://twitter.com/dsolvd",
+      "icon_classes": "fab fa-twitter"
+    },
+    {
+      "Title": "Youtube",
+      "URL": "https://www.youtube.com/c/Dissolvedd",
+      "icon_classes": "fab fa-youtube"
+    },
+  ]
+}
