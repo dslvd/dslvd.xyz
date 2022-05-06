49d5baf931fbccf510f4ed24ce500867e75170df
dslvd <80934874+dslvd@users.noreply.github.com>
2022-05-06 18:24:11 +0800

    navi page for movies

---
+++ movies/assets/config.js
@@ -0,0 +1,22 @@
+let config = {
+  "Title": "",
+  "Name": "",
+  "Description": "",
+  "links": [
+    {
+      "Title": "Sonic the Hedgehog 2",
+      "URL": "https://dslvd.xyz/movies/Sonic-the-Hedgehog-2",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Spider Man No Way Home",
+      "URL": "https://dslvd.xyz/movies/Spider-Man-No-Way-Home",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "The Batman",
+      "URL": "https://dslvd.xyz/movies/The-Batman",
+      "icon_classes": "fas fa-link"
+    },
+  ]
+}
