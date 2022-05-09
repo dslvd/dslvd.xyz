797d9c241f046c5d465072e0891d0b48dd9be1ab
dslvd <80934874+dslvd@users.noreply.github.com>
2022-05-09 22:58:54 +0800

    new movies zamn

--- movies/assets/config.js
+++ movies/assets/config.js
@@ -18,5 +18,20 @@ let config = {
       "URL": "https://dslvd.xyz/movies/The-Batman",
       "icon_classes": "fas fa-link"
     },
+    {
+      "Title": "Uncharted 2022",
+      "URL": "https://dslvd.xyz/movies/Uncharted-2022",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "The Bad Guys",
+      "URL": "https://dslvd.xyz/movies/The-Bad-Guys",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Tom and Jerry 2021",
+      "URL": "https://dslvd.xyz/movies/Tom-and-Jerry-2021",
+      "icon_classes": "fas fa-link"
+    },
   ]
 }
