b02289135b6bd64669a0634d3a61f364dcf6d599
dslvd <80934874+dslvd@users.noreply.github.com>
2022-05-21 15:03:45 +0800

    new 404

--- assets/404/script.js
+++ assets/404/script.js
@@ -1,67 +1,34 @@
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 5,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#fcfcfc"
-    },
-    "shape": {
-      "type": "circle",
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.2,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 140,
-      "random": false,
-      "anim": {
-        "enable": true,
-        "speed": 10,
-        "size_min": 40,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-    },
-    "move": {
-      "enable": true,
-      "speed": 8,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-    "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false
-      },
-      "onclick": {
-        "enable": false
-      },
-      "resize": true
-    }
-  },
-  "retina_detect": true
-});
+var $copyContainer = $(".copy-container"),
+    $replayIcon = $('#cb-replay'),
+    $copyWidth = $('.copy-container').find('p').width();
+
+var mySplitText = new SplitText($copyContainer, {type:"words"}),
+    splitTextTimeline = new TimelineMax();
+var handleTL = new TimelineMax();
+
+var tl = new TimelineMax();
+
+tl.add(function(){
+  animateCopy();
+  blinkHandle();
+}, 0.2)
+
+function animateCopy() {
+  mySplitText.split({type:"chars, words"}) 
+  splitTextTimeline.staggerFrom(mySplitText.chars, 0.001, {autoAlpha:0, ease:Back.easeInOut.config(1.7), onComplete: function(){
+    animateHandle()
+  }}, 0.05);
+}
+
+function blinkHandle() {
+  handleTL.fromTo('.handle', 0.4, {autoAlpha:0},{autoAlpha:1, repeat:-1, yoyo:true}, 0);
+}
+
+function animateHandle() {
+  handleTL.to('.handle', 0.7, {x:$copyWidth, ease:SteppedEase.config(12)}, 0.05);
+}
+
+$('#cb-replay').on('click', function(){
+  splitTextTimeline.restart()
+  handleTL.restart()
+})
