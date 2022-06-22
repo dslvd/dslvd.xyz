4a2e75a8e106d8e57b4755a0e713507c2ec4641a
dslvd <80934874+dslvd@users.noreply.github.com>
2022-06-22 21:35:05 +0800

    jadey <3

---
+++ jadey/assets/config.js
@@ -0,0 +1,17 @@
+let config = {
+  "Title": "",
+  "Name": "",
+  "Description": "",
+  "links": [
+    {
+      "Title": "Instagram",
+      "URL": "https://dslvd.xyz/jadey/instagram",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Tiktok",
+      "URL": "https://dslvd.xyz/jadey/tiktok",
+      "icon_classes": "fas fa-link",
+    },
+  ]
+}
